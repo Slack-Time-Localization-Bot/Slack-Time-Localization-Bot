6938e1f7066c6f2c678b56143fe71e5189c92e93
Philipp Hossner <philipp.hossner@posteo.de>
2024-02-25 20:42:58 +0100

    Add tests for message handling and CLI entrypoint (#9)

--- slack_time_localization_bot/config.py
+++
@@ -1,9 +0,0 @@
-import logging
-import os
-
-TIME_FORMAT = "%H:%M"
-LOG_LEVEL = logging.INFO
-SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
-SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
-USER_CACHE_SIZE = 500
-USER_CACHE_TTL = 600
